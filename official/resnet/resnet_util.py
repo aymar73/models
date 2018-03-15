5c1e2c8cd780e6d2b67b179d9da5a02c848bea93
Taylor Robie <taylorrobie@google.com>
2018-03-15 15:19:05 -0700

    alter resnet file structure

--- official/resnet/resnet_util.py
+++ official/resnet/resnet_util.py
@@ -12,19 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Contains definitions for Residual Networks.
-
-Residual networks ('v1' ResNets) were originally proposed in:
-[1] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
-    Deep Residual Learning for Image Recognition. arXiv:1512.03385
-
-The full preactivation 'v2' ResNet variant was introduced by:
-[2] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
-    Identity Mappings in Deep Residual Networks. arXiv: 1603.05027
-
-The key difference of the full preactivation 'v2' variant compared to the
-'v1' variant in [1] is the use of batch normalization before every weight layer
-rather than after.
+"""Contains utility and supporting functions for ResNet.
 """
 
 from __future__ import absolute_import
